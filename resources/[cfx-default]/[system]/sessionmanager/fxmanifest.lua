3707582906a91869456694c523e6519dde98d699
Gaming-BigD <gamingbigd88@gmail.com>
2023-09-08 13:40:00 +0200

    esx

---
+++ resources/[cfx-default]/[system]/sessionmanager/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+version '1.0.0'
+author 'Cfx.re <root@cfx.re>'
+description 'Handles the "host lock" for non-OneSync servers. Do not disable.'
+repository 'https://github.com/citizenfx/cfx-server-data'
+
+
+
+server_script 'server/host_lock.lua'
+client_script 'client/empty.lua'
